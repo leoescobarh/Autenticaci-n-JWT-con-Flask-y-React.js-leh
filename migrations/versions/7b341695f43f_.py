5df729811324978b5001fb57d7ad7421273296be
leoescobarh <ldescobarherrera@gmail.com>
2025-05-12 17:49:17 +0000

    autenticacion

--- migrations/versions/7b341695f43f_.py
+++ migrations/versions/7b341695f43f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0763d677d453
+Revision ID: 7b341695f43f
 Revises: 
-Create Date: 2025-02-25 14:47:16.337069
+Create Date: 2022-08-31 21:29:18.243707
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0763d677d453'
+revision = '7b341695f43f'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('email', sa.String(length=120), nullable=False),
-    sa.Column('password', sa.String(), nullable=False),
+    sa.Column('password', sa.String(length=80), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,12 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
 
 api = Blueprint('api', __name__)
-
-# Allow CORS requests to this API
-CORS(api)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -20,3 +16,4 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+    
